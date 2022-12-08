8ad0d9d94f5f61c55c1440e4e862ba28c85902e1
Isshak Darwich <isshak.mohamad@gmail.com>
2022-12-08 18:30:52 -0300

    Create quadrado-colorido.js

---
+++ quadrado-colorido.js
@@ -0,0 +1,7 @@
+function verde(){
+	document.getElementById("quadrado-colorido").style.backgroundColor = "green";
+}
+
+function amarelo(){
+	document.getElementById("quadrado-colorido").style.backgroundColor = "yellow";
+}
